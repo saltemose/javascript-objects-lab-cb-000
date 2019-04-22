89960046dadea32f86831d344045cca1d42965a0
Sean Altemose <sean@seanaltemose.com>
2019-04-22 16:39:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 };
 
 function deleteFromObjectByKey(object, key) {
+  var newObject = object
   delete object[key]
   return object
 };
